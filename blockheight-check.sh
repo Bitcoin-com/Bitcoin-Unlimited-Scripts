51c677259c1194d2392f0a1e660a8ddb01f1e2c2
magmahindenburg <emil@moar.se>
2017-03-27 10:40:57 +0900

    Added script for changing MG setting in BU

---
+++ blockheight-check.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+rpcpassword=$(cat /etc/bitcoin/bitcoin.conf | grep rpcpassword | cut -d '=' -f2)
+rpcuser=$(cat /etc/bitcoin/bitcoin.conf | grep rpcuser | cut -d '=' -f2)
+
+blockheight=$(/usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword getblockcount)
+
+echo "Current height: $blockheight"
+
+currentmaxblock=$(/usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword getexcessiveblock | grep excessiveBlockSize | grep -o [0-9]*)
+currentminedblock=$(/usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword getminingmaxblock)
+
+# This was tested on testnet. Set a proper height for mainnet
+if [ "$blockheight" -ge "1092300" ]; then
+    if [ "$currentmaxblock" -lt "2000000" ]; then
+        /usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword setexcessiveblock 9000000 10
+	# If you have a script for changing the coinbase message, set it here
+    fi
+    if [ "$currentminedblock" -lt "2000000" ]; then
+        /usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword setminingmaxblock 4000000
+	# If you have a script for changing the coinbase message, set it here
+    fi
+fi
