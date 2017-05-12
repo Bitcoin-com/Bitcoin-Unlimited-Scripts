a1f4ed73fccd0b23c5d842bc40b887f2004e02a1
Magma Hindenburg <emil@moar.se>
2017-05-12 16:42:13 +0900

    Update blockheight-check.sh

--- blockheight-check.sh
+++ blockheight-check.sh
@@ -9,14 +9,13 @@ echo "Current height: $blockheight"
 currentmaxblock=$(/usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword getexcessiveblock | grep excessiveBlockSize | grep -o [0-9]*)
 currentminedblock=$(/usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword getminingmaxblock)
 
-# This was tested on testnet. Set a proper height for mainnet
-if [ "$blockheight" -ge "1092300" ]; then
-    if [ "$currentmaxblock" -lt "2000000" ]; then
-        /usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword setexcessiveblock 2000000 12
+if [ "$blockheight" -ge "488888" ]; then
+    if [ "$currentmaxblock" -lt "8000000" ]; then
+        /usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword setexcessiveblock 8000000 12
 	# If you have a script for changing the coinbase message, set it here
     fi
-    if [ "$currentminedblock" -lt "2000000" ]; then
-        /usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword setminingmaxblock 2000000
+    if [ "$currentminedblock" -lt "8000000" ]; then
+        /usr/local/bin/bitcoin-cli -rpcuser=$rpcuser -rpcpassword=$rpcpassword setminingmaxblock 8000000
 	# If you have a script for changing the coinbase message, set it here
     fi
 fi
